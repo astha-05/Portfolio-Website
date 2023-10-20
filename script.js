f0618fa9edc4c975a70c74db6fc66e1997fcd7bb
astha-05 <121555529+astha-05@users.noreply.github.com>
2023-10-20 09:19:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+    
+
+// Progress bars
+$(document).ready(function() {
+    $('.progress .progress-bar').css("width",
+              function() {
+                  return $(this).attr("aria-valuenow") + "%";
+              }
+      )
+  });
